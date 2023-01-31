c209c575ca4f9d024909affb35b12160cbb88de2
kquac00 <kimlyq@gmail.com>
2023-01-30 21:39:13 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function clicked(element){
+    element.innerText = "Logout";
+}
+
+function removeText(element){
+    element.remove();
+}
+
+function likedText(element) {
+    alert( " This page says Ninja was liked ");
+}
